47a2767c772681c434f90ba48031ad29685b83d4
Daniel Medina <daniel.medina11@protonmail.com>
2017-07-29 21:59:00 +0200

    working on unit tests

--- test/backend.js
+++ test/backend.js
@@ -4,15 +4,44 @@
  */
 
 /** Nothing needs to be asynchronous here */
-const request = require('request')
-// const expect = require('chai').expect
+const chai = require('chai')
+const expect = chai.expect
+const http = require('chai-http')
+const server = 'http://localhost:3000'
+
+chai.use(http)
 
 describe('Core parts', function () {
-  it('test ?! onche onche', function () {
-    request('http://localhost:3000', function (error, response, body) {
-      console.log(error)
-      console.log(response)
-      console.log(body)
+  describe('Verifying that all pages are properly sent to the user', function () {
+    it('Index page is properly sent to the guest', function (done) {
+      /**
+       * See if the index page returns a http message 200 or not
+       *
+       * @async
+       * @returns {Promise} Promise containing the request on the server
+       */
+      async function getStatus () {
+        return chai.request(server)
+          .get('/')
+          .end()
+      }
+
+      /**
+       * Asynchronous code execution
+       *
+       * @async
+       * @throws Will throw an error to the console if it catches one
+       */
+      (async function () {
+        try {
+          const status = await getStatus()
+
+          expect(status).to.equal(200)
+          done()
+        } catch (error) {
+          console.log(error)
+        }
+      }())
     })
   })
 })
