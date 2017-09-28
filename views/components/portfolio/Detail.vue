bcea60312ba7c79978fc3b01bff2f83a96075f0d
Daniel Medina <daniel.medina11@protonmail.com>
2017-09-28 15:43:25 +0200

    done project's modal frontend

--- views/components/portfolio/Detail.vue
+++ views/components/portfolio/Detail.vue
@@ -4,7 +4,44 @@
     .window.col-sm-10.col-md-10.col-lg-10.col-xs-12
       .buttons
         i.fa.fa-window-close(@click="$parent.display()")
-      | Test
+        .name
+          | #[i.fa.fa-arrow-circle-o-right] Project's name
+      .images-wrapper.col-md-7.col-lg-7.col-xs-12
+        .images
+          img(src='https://www.oxalide.com/wp-content/uploads_prod/2014/02/nodejs-new-pantone-black.png').col-md-12
+          .to-left
+            i.fa.fa-chevron-left
+          .to-right
+            i.fa.fa-chevron-right
+      .information.col-md-5.col-xs-12
+        .category
+          .title
+            | Description #[i.fa.fa-info-circle]
+          .description
+            | Test
+
+        .category
+          .title
+            | Languages #[i.fa.fa-tags]
+          .tags
+            li #[button.green='JavaScript']
+            li #[button.red='PHP']
+            li #[button.black='css']
+            li #[button.grey='python']
+        .category
+          .title
+            | Frameworks #[i.fa.fa-tags]
+          .tags
+            li #[button.green='Node.JS']
+            li #[button.red='Laravel']
+            li #[button.orange='React']
+            li #[button.pink='SASS']
+        .category
+          .title
+            | Links #[i.fa.fa-external-link]
+          .links
+            a.button.btn.btn-xs.btn-success(href='#', target='_blank') Access
+            a.button.pull-right.btn.btn-xs.btn-danger(href='#', target='_blank') Source code
 </template>
 
 <script>
@@ -65,6 +102,7 @@ export default {
 /** Should be changed to a "modal" or "popup" class someday */
 #detail {
   .wrapper {
+    background: $popup-background;
     transition: $transition;
     position: fixed;
     display: flex;
@@ -75,24 +113,176 @@ export default {
     left: 0;
     width: 100%;
     height: 100%;
+    overflow: auto;
 
     .window {
+      overflow-x: hidden;
+      overflow-y: auto;
       padding: 0;
       border: $portfolio-project-popup-border;
       background: $portfolio-project-popup-background;
+      box-shadow: $popup-shadow;
       height: $portfolio-project-popup-height;
 
       .buttons {
+        position: absolute;
         text-align: right;
         padding-right: $portfolio-project-popup-buttons-padding;
         font-size: $portfolio-project-popup-buttons-size;
         width: 100%;
+        z-index: 999999;
 
         i {
           transition: $transition;
           &:hover {
             cursor: pointer;
             color: $a-hover-color;
+          }
+        }
+      }
+      .name {
+        z-index: 999999;
+        text-align: left;
+        overflow: hidden;
+        max-height: 80px;
+        position: absolute;
+        background: $color9;
+        color: $color1;
+        top: $portfolio-project-name-margin;
+        width: 100%;
+        padding: 5px;
+        font-family: $font-all;
+        font-weight: bold;
+        font-size: $font-big;
+        text-transform: uppercase;
+      }
+
+      .images-wrapper {
+        display: flex;
+        align-items: center;
+        height: 100%;
+
+        .images {
+          display: flex;
+          position: relative;
+          overflow: hidden;
+          align-items: center;
+
+          min-height: $portfolio-project-detail-image-min-height;
+          max-height: $portfolio-project-detail-image-max-height;
+          height: auto;
+          width: 100%;
+
+          img {
+            width: 100%;
+          }
+          .to-left {
+            position: absolute;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            top: 0;
+            left: 0;
+
+            width: $popup-chevron-width;
+            height: 100%;
+
+            background: $popup-chevron-background;
+            color: $popup-chevron-color;
+            font-size: $popup-chevron-size;
+
+            &:hover {
+              cursor: pointer;
+            }
+          }
+
+          .to-right {
+            position: absolute;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            top: 0;
+            right: 0;
+
+            width: $popup-chevron-width;
+            height: 100%;
+
+            background: $popup-chevron-background;
+            color: $popup-chevron-color;
+            font-size: $popup-chevron-size;
+
+            &:hover {
+              cursor: pointer;
+            }
+          }
+
+        }
+      }
+      .information {
+        margin-top: $portfolio-project-information-margin;
+
+        .category {
+          margin-top: $portfolio-project-information-category-margin;
+          .title {
+            text-align: right;
+            text-transform: uppercase;
+            font-weight: bold;
+            font-size: $portfolio-project-information-category-title-font-size;
+            border-bottom: $portfolio-project-information-category-title-border;
+          }
+          .tags {
+            margin-top: $portfolio-project-information-category-tags-margin;
+            li {
+              display: inline;
+              list-style: none;
+              button {
+                color: white;
+                font-weight: bold;
+                font-size: $font-tiny;
+                text-transform: uppercase;
+                border: $portfolio-project-information-tags-border;
+                border-radius: 4px;
+
+                margin: $portfolio-project-information-tags-margin;
+              }
+
+              /** Different colors for different tags */
+              .green {
+                background: $portfolio-project-information-tags-background-green;
+                border: $portfolio-project-information-tags-border-green;
+              }
+              .red {
+                background: $portfolio-project-information-tags-background-red;
+                border: $portfolio-project-information-tags-border-red;
+              }
+              .grey {
+                background: $portfolio-project-information-tags-background-grey;
+                border: $portfolio-project-information-tags-border-grey;
+              }
+              .orange {
+                background: $portfolio-project-information-tags-background-orange;
+                border: $portfolio-project-information-tags-border-orange;
+              }
+              .pink {
+                background: $portfolio-project-information-tags-background-pink;
+                border: $portfolio-project-information-tags-border-pink;
+              }
+              .black {
+                background: $portfolio-project-information-tags-background-black;
+                border: $portfolio-project-information-tags-border-black;
+              }
+            }
+          }
+          .links {
+            margin-top: $portfolio-project-information-category-tags-margin;
+            list-style: none;
+          }
+          .description {
+            word-break: break-all;
+            font-size: $font-small;
+            font-family: $font-read;
           }
         }
       }
